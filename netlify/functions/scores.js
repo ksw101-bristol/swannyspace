7742ad12bddbda2da10fb8d7926fbb4c2ebb6fce
ksw101-bristol <kirkseanwalsh@gmail.com>
2026-06-24 01:28:22 +0100

    Create scores.js

---
+++ netlify/functions/scores.js
@@ -0,0 +1,34 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event) {
+  const apiKey = process.env.API_FOOTBALL_KEY;
+  
+  try {
+    const response = await fetch(
+      'https://v3.football.api-sports.io/fixtures?live=all&league=1&season=2026',
+      {
+        headers: {
+          'x-rapidapi-host': 'v3.football.api-sports.io',
+          'x-rapidapi-key': apiKey
+        }
+      }
+    );
+    
+    const data = await response.json();
+    
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+    
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to fetch scores' })
+    };
+  }
+};
